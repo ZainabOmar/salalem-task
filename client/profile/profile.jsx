855a4bb55352e136f762ed7763369ef101cc3203
Zainab Hammami <zainab.hammami.zh@gmail.com>
2017-05-14 00:09:58 +0300

    PROFILE

--- client/profile/profile.jsx
+++ client/profile/profile.jsx
@@ -1,89 +1,73 @@
-<div class="row" style="
-background: url('assets/inside-bg.jpg');
-background-position: top;
-min-height: 500px;
-z-index: 2;
-padding-top: 100px;
-" ng-controller="ProfileController" ng-init="getProfile()">
-
-<div class="row" style="padding-bottom: 50px;margin:0px;"><div class="col-md-10 col-md-offset-1 text-center">
-
-  <div class="col-md-4 col-xs-12" style="color: #fff;margin-top: 20px;">
-
-  <img ng-if="data.ImgUrl === null" src="/assets/chef.png" class="img-responsive thumbnail" style="margin: auto;">
-  <img ng-if="data.ImgUrl !== null" src="/{{ data.ImgUrl }}" class="img-responsive thumbnail" style="margin: auto;">
-
-</div>
-
-<div class="col-md-8 col-xs-12" style="color: #fff;margin-top: 20px;">
-
-  <h1 style="font-weight: bold;color: #fff;text-align: left;margin-bottom: 40px;">
-  {{ data.FullName }}</h1>
-
-  <div class="col-md-6 col-xs-12">
-    <i class="glyphicon glyphicon-phone" style="font-size: 3em;"></i>
+var Profile = (props) => (
+  <div className="row" style={{
+    background: "url('assets/inside-bg.jpg')",
+    backgroundPosition: 'top',
+    minHeight: '500px',
+    zIndex: '2',
+    paddingTop: '100px'}}>
+    <div className="row" style={{paddingBottom: '50px', margin: '0px'}}>
+    <div className="col-md-10 col-md-offset-1 text-center">
+    <div className="col-md-4 col-xs-12" style={{ color: '#fff', marginTop: '20px'}}>
+    <img src="/assets/chef.png" className="img-responsive thumbnail" style={{margin: 'auto'}} />
+    <img src="/ImgUrl" className="img-responsive thumbnail" style={{margin: 'auto'}}/>
+    </div>
+    <div className="col-md-8 col-xs-12" style={{color: '#fff', marginTop: '20px'}}>
+    <h1 style={{fontWeight: 'bold', color: '#fff', textAlign: 'left', marginBottom: '40px'}}>
+    data FullName </h1>
+    <div className="col-md-6 col-xs-12">
+    <i className="glyphicon glyphicon-phone" style="font-size: 3em;"></i>
     <p style="line-height: 40px;font-size: 15pt;">
-    {{ data.PhoneNumber }}
+    data PhoneNumber 
     </p>
-  </div>
-
-
-  <div class="col-md-6 col-xs-12">
-    <i class="glyphicon glyphicon-envelope" style="font-size: 3em;"></i>
-    <p style="line-height: 40px;font-size: 15pt;">{{ data.Email }}</p>
-  </div>
-
-</div>
-
-<div class="col-xs-12" style="margin-top: 50px;" ng-if="data.todayCook !== ''">  
-  <span style="padding: 20px;font-size: 1.7em;color: #fff;"><b>{{ data.FullName }}</b> cooking for today is : <b>{{ data.todayCook.Name }}</b>, just for 
-  <b>{{ data.todayCook.Price }}</b> JOD!</span>
-  <button ng-click="setOrder($root.UserID, data.todayCook.ID, 1, data.todayCook.FullName, data.todayCook.Name)" class="btn btn-lg btn-primary">Order now</button>
-</div>
-
-</div>
-</div>
-
-
-</div>
-
-
-<div class="row" style="margin-top: 40px;">
-
-<div class="col-md-10 col-md-offset-1 col-xs-12">
-
-
-  <div class="panel panel-default">
-    <div class="panel-heading"><b>{{ data.user.FullName }}</b> schedule</div>
-
-    <table class="table">
-      <tbody>
-      <tr style="font-weight: bold;">
-        <td>Day</td>
-        <td ng-repeat="cook in data.schedule">{{ cook.DayName }}</td>
-      </tr>
-      <tr>
-        <td style="font-weight: bold;">Cooking</td>
-        <td ng-repeat="cook in data.schedule">{{ cook.CookeName }}</td>
-      </tr>
+    </div>
+    <div className="col-md-6 col-xs-12">
+    <i className="glyphicon glyphicon-envelope" style={{fontSize: '3em'}}></i>
+    <p style={{lineHeight: '40px', fontSize: '15pt'}}> data Email </p>
+    </div>
+    </div>
+    <div className="col-xs-12" style={{marginTop: '50px'}}>  
+    <span style={{padding: '20px', fontSize: '1.7em', color: '#fff'}}><b> data FullName </b> cooking for today is : <b> data todayCook Name </b>, just for 
+    <b> data todayCook Price </b> JOD!</span>
+    <button className="btn btn-lg btn-primary">Order now</button>
+    </div>
+    </div>
+    </div>
+    </div>
+    <div className="row" style= {{marginTop: '40px'}}>
+    <div className="col-md-10 col-md-offset-1 col-xs-12">
+    <div className="panel panel-default">
+    <div className="panel-heading"><b> data user FullName </b> schedule</div>
+    <table className="table">
+    <tbody>
+    <tr style={{fontWeight: 'bold'}}>
+    <td>Day</td>
+    <td> cook DayName </td>
+    </tr>
+    <tr>
+    <td style={{fontWeight: 'bold'}}>Cooking</td>
+    <td> cook CookeName </td>
+    </tr>
     </tbody></table>
-  </div>
-</div>
-</div>
-<div class="row">
-  <div class="page-header text-center">
-    <h1>User reviews <small> for {{ data.FullName }}</small></h1>
-  </div>
-</div>
-<div class="row">
-  <div class="col-md-10 col-md-offset-1 col-xs-12">
+    </div>
+    </div>
+    </div>
+    <div className="row">
+    <div className="page-header text-center">
+    <h1>User reviews <small> for data FullName </small></h1>
+    </div>
+    </div>
+    <div className="row">
+    <div className="col-md-10 col-md-offset-1 col-xs-12">
 
-    <div class="col-md-6 col-xs-12" ng-repeat="comment in data.comments">
-      <div class="panel panel-default">
-        <div class="panel-body">{{ comment.ComBody }}</div>
-        <div class="panel-footer">{{ comment.InsertedUserFullName }}</div>
-      </div>
+    <div className="col-md-6 col-xs-12">
+    <div className="panel panel-default">
+    <div className="panel-body"> comment ComBody </div>
+    <div className="panel-footer"> comment InsertedUserFullName </div>
+    </div>
     </div>
 
-  </div>
-  </div>
+    </div>
+    </div>
+    )
+
+window.Profile = Profile;
